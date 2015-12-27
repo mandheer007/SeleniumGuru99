aea7dcca03aef8a97e29e46e311a2e4b8ffc3037
Manu <Manu@Manu-PC>
2015-12-27 14:02:27 +0530

    Second Commit - added Util.java Class

--- SeleniumGuru99/src/guru99bank/LoginExample.java
+++ SeleniumGuru99/src/guru99bank/LoginExample.java
@@ -9,13 +9,18 @@ public class LoginExample {
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Util utl = new Util();
+		String uname = utl.username;
+		String psswd = utl.password;
+		
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.demo.guru99.com/V4/");
-		driver.findElement(By.name("uid")).sendKeys("mngr26059");
-		driver.findElement(By.name("password")).sendKeys("EzatygA");
+		driver.findElement(By.name("uid")).sendKeys(uname);
+		driver.findElement(By.name("password")).sendKeys(psswd);
 		driver.findElement(By.name("btnLogin")).click();
 		
 		driver.close();
---
+++ SeleniumGuru99/src/guru99bank/Util.java
@@ -0,0 +1,12 @@
+package guru99bank;
+
+public class Util {
+
+	/**
+	 * @param args
+	 */
+ public String username = "mngr26059";
+ public String password = "EzatygA";
+
+ 
+}
